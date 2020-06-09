8be013e380412d67fb423eeb87eea34e1c6a938a
valentina-b <valentina.bedi@gmail.com>
2020-06-09 17:12:40 +0000

    Finish crud ui

--- mongo_project.py
+++ mongo_project.py
@@ -1,9 +1,14 @@
 import pymongo
 import os
 
+from os import path
+if path.exists("env.py"):
+    import env
+
 MONGODB_URI = os.getenv("MONGO_URI")
 DBS_NAME = "myTestDB"
 COLLECTION_NAME = "myFirstMDB"
+
 
 def mongo_connect(url):
     try:
@@ -53,12 +58,10 @@ def add_record():
     occupation = input("Enter occupation > ")
     nationality = input("Enter nationality > ")
 
-    # build the dictionary from the inputs above
     new_doc = {'first': first.lower(), 'last': last.lower(), 'dob': dob,
                'gender': gender, 'hair_colour': hair_colour, 'occupation':
                occupation, 'nationality': nationality}
 
-    # then do try except block
     try:
         coll.insert(new_doc)
         print("")
@@ -67,21 +70,85 @@ def add_record():
         print("Error accessing the database")
 
 
+def find_record():
+    doc = get_record()
+    if doc:
+        print("")
+        # The first thing we want to check is if the key is not equal to ID.
+        # ID, if you remember, is that default key that is created by Mongo.
+        # We don't want that to be displayed.
+        # So if the key is not equal to ID, then let's print the key.
+        for k, v in doc.items():
+            if k != "_id":
+                print(k.capitalize() + ": " + v.capitalize())
+
+
+def edit_record():
+    doc = get_record()
+    if doc:
+        update_doc = {}
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                # So what we're going to do this time, then, is that as we're iterating through,
+                # we're going to add our update_doc dictionary.
+                # So we're going to provide the key for our update_doc dictionary.
+                # And the value for that is going to be equal to our input here.
+                # So we're going to capitalize the k as our prompt.
+                # I'm going to put a + "["+ v +"]
+                # And the reason that we're doing this is that we want the value to appear
+                # in these square brackets so that we can see what the current value of them is.
+                # So k is the key, and v is the value.
+                # We should get a prompt that contains both the key and what the value is currently set to.# 
+                update_doc[k] = input(k.capitalize() + " [" + v + "] > ")
+
+                # If we haven't actually entered anything for update_doc, if we've just left it blank,
+                # we don't actually want to delete the information that's in there.
+                # We just want to leave it the same as it was before.
+                if update_doc[k] == "":
+                    update_doc[k] = v
+
+        try:
+            coll.update_one(doc, {'$set': update_doc})
+            print("")
+            print("Document updated")
+        except:
+            print("Error accessing the database")
+
+
+def delete_record():
+
+    doc = get_record()
+
+    if doc:
+        print("")
+        for k, v in doc.items():
+            if k != "_id":
+                print(k.capitalize() + ": " + v.capitalize())
+
+        print("")
+        confirmation = input("Is this the document you want to delete?\nY or N > ")
+        print("")
+
+        if confirmation.lower() == 'y':
+            try:
+                coll.remove(doc)
+                print("Document deleted!")
+            except:
+                print("Document not deleted")
+
 
 def main_loop():
     while True:
         option = show_menu()
         if option == "1":
-            # print("You have selected option 1")
             add_record()
-
-
         elif option == "2":
-            print("You have selected option 2")
+            find_record()
         elif option == "3":
-            print("You have selected option 3")
+            edit_record()
         elif option == "4":
-            print("You have selected option 4")
+            delete_record()
         elif option == "5":
             conn.close()
             break
