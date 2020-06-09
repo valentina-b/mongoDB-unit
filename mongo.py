f71a4a0cd8712d767ed7134af6a8e1ffcbaf04f6
valentina-b <valentina.bedi@gmail.com>
2020-06-09 07:34:53 +0000

    CRUD with python

--- mongo.py
+++ mongo.py
@@ -23,7 +23,39 @@ conn = mongo_connect(MONGODB_URI)
 
 coll = conn[DBS_NAME][COLLECTION_NAME]
 
-documents = coll.find()
+# # insert a new record
+# # save it in a var
+# new_doc = {'first': 'douglas', 'last': 'adams', 'dob': '11/03/1952', 'hair_colour': 'grey', 'occupation': 'writer', 'nationality': 'english'}
 
+# coll.insert(new_doc)
+
+
+
+# # what if we want to add more than one record?
+# # create a new var
+# # send an array of dictionaries
+# new_docs = [{'first': 'terry', 'last': 'pratchett', 'dob': '28/04/1948', 'gender': 'm', 'hair_colour': 'not much', 'occupation': 'writer', 'nationality': 'english'}, {'first': 'george', 'last': 'rr martin', 'dob': '20/09/1948', 'gender': 'm', 'hair_colour': 'white', 'occupation': 'writer', 'nationality': 'american'}]
+
+# coll.insert_many(new_docs)
+
+
+
+
+# # how do we find specific data?
+# # in find() send in key calue pairs in a dictionary of what it is that we're searching for
+# documents = coll.find({'first': 'douglas'})
+
+
+
+# update one
+coll.update_one({'nationality': 'american'}, {'$set': {'hair_colour': 'maroon'}})
+
+documents = coll.find({'nationality': 'american'})
+
+
+
+# documents = coll.find()
+
+# how to print the object line by line
 for doc in documents:
     print(doc)
